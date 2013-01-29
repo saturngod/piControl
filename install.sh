140788d371fb94aa6c1bbe65f203a2e1779d4a2d
saturngod <saturngod@gmail.com>
2013-01-29 14:27:35 +0800

    update nodeinstall.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd /usr/src
-sudo wget http://nodejs.org/dist/v0.8.18/node-v0.8.18.tar.gz
-sudo tar xvzf node-v0.8.18.tar.gz
+wget http://nodejs.org/dist/v0.8.18/node-v0.8.18.tar.gz
+tar xvzf node-v0.8.18.tar.gz
 cd node-v0.8.18
 ./configure
 make
